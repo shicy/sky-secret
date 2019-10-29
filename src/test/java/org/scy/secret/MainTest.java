5c4fdcaba1a574c1a689029261104442886f7aef
shicy <shicy85@163.com>
2019-10-29 10:07:15 +0800

    测试

---
+++ src/test/java/org/scy/secret/MainTest.java
@@ -0,0 +1,17 @@
+package org.scy.secret;
+
+import com.alibaba.fastjson.JSON;
+import org.scy.common.web.model.ValidInfo;
+
+public class MainTest {
+
+    public static void main(String[] args) {
+        ValidInfo validInfo = new ValidInfo();
+        validInfo.setCode("jfej23Ff");
+        validInfo.setImageUrl("交话费文化减肥u我就佛i额外");
+
+        String json = JSON.toJSONString(validInfo);
+        System.out.println(json);
+    }
+
+}
