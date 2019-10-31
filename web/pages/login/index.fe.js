76e385e1de1511a1d2687018d6daae54900adc50
shicy <shicy85@163.com>
2019-10-31 17:37:01 +0800

    登录页面

--- web/pages/login/index.fe.js
+++ web/pages/login/index.fe.js
@@ -3,6 +3,23 @@
  * Created by shicy 2019-10-28
  */
 
+const loginView = $(".container.login");
+const registerView = $(".container.register");
+
+///////////////////////////////////////////////////////////
+// 点击 前往注册 按钮
+loginView.on("tap", "[name=toRegister]", function () {
+	loginView.hide();
+	registerView.show();
+});
+
+// 点击 登录 按钮
+registerView.on("tap", "[name=toLogin]", function () {
+	registerView.hide();
+	loginView.show();
+});
+
+// ========================================================
 // 更新验证码
 const refreshValidCode = function (target) {
 	VR.fetch("login.validcode", (err, ret) => {
