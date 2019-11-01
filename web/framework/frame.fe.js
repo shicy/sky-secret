5509fd2c5b9d7a0720ee31d74a97e94defe37be8
shicy <shicy85@163.com>
2019-11-01 16:45:10 +0800

    添加 frame.tooltip()

--- web/framework/frame.fe.js
+++ web/framework/frame.fe.js
@@ -6,3 +6,10 @@
 const frame = window.frame = function () {};
 
 ///////////////////////////////////////////////////////////
+frame.tooltip = function (type, message, callback) {
+	if (type == "error")
+		type = "danger";
+	let _tooltip = new UIMessage({type: type, content: message});
+	if (Utils.isFunction(callback))
+		_tooltip.on("close", callback);
+};
