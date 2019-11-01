4849023a5f9c778af55d6c5e279bc6b28b5dab73
shicy <shicy85@163.com>
2019-11-01 16:44:46 +0800

    新用户注册接口

--- src/main/java/org/scy/secret/controller/UserController.java
+++ src/main/java/org/scy/secret/controller/UserController.java
@@ -1,13 +1,22 @@
 package org.scy.secret.controller;
 
+import org.apache.commons.lang3.StringUtils;
+import org.scy.common.utils.CommonUtilsEx;
 import org.scy.common.web.controller.BaseController;
 import org.scy.common.web.controller.HttpResult;
 import org.scy.common.web.model.ValidInfo;
+import org.scy.common.web.session.LoginForm;
 import org.scy.common.web.session.SessionManager;
+import org.scy.priv.PrivilegeClientAdapter;
+import org.scy.priv.model.User;
+import org.scy.secret.form.RegisterForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 用户相关
@@ -22,10 +31,52 @@ public class UserController extends BaseController {
      */
     @RequestMapping(value = "/validcode", method = RequestMethod.GET)
     public Object getValidCode() {
-        ValidInfo validInfo = SessionManager.getValidCode();
+        ValidInfo validInfo = SessionManager.getValidateInfo();
         if (validInfo != null)
             return HttpResult.ok(validInfo);
-        return HttpResult.error(401, "获取验证码失败");
+        return HttpResult.error(1001, "获取验证码失败");
+    }
+
+    public Object login(LoginForm loginForm) {
+        return null;
+    }
+
+    /**
+     * 新用户注册
+     * @param registerForm 注册信息
+     * @return 返回用户信息
+     */
+    @RequestMapping(value = "/user/register", method = RequestMethod.POST)
+    public Object register(HttpServletRequest request, HttpServletResponse response, RegisterForm registerForm) {
+        if (registerForm == null)
+            return HttpResult.error(1002, "没有用户信息");
+
+        String validCodeId = registerForm.getValidCodeId();
+        String validCode = registerForm.getValidCode();
+        if (StringUtils.isBlank(validCodeId) || StringUtils.isBlank(validCode))
+            return HttpResult.error(1003, "验证码无效");
+        boolean isUserValid = SessionManager.checkValidateCode(validCodeId, validCode);
+        if (!isUserValid)
+            return HttpResult.error(1003, "验证码无效");
+
+        if (StringUtils.isBlank(registerForm.getMobile()))
+            return HttpResult.error(1004, "手机号码不能为空");
+        if (CommonUtilsEx.checkMobile(registerForm.getMobile()))
+            return HttpResult.error(1004, "请输入正确的手机号码");
+
+        User user = new User();
+        user.setName(registerForm.getUsername());
+        user.setMobile(registerForm.getMobile());
+        user.setEmail(registerForm.getEmail());
+        user.setPassword(registerForm.getPassword());
+
+        user = PrivilegeClientAdapter.addUser(user, 0, 0);
+        if (user != null) {
+            String token = SessionManager.doLoginWithoutPassword(registerForm.getMobile());
+            SessionManager.setUser(token, user.toPlatUser(), response);
+        }
+
+        return HttpResult.ok(user);
     }
 
 }
---
+++ src/main/java/org/scy/secret/form/RegisterForm.java
@@ -0,0 +1,63 @@
+package org.scy.secret.form;
+
+/**
+ * 注册表单
+ * Created by shicy 2019/10/31
+ */
+public class RegisterForm {
+
+    private String username;
+    private String mobile;
+    private String email;
+    private String password;
+    private String validCode;
+    private String validCodeId;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getValidCode() {
+        return validCode;
+    }
+
+    public void setValidCode(String validCode) {
+        this.validCode = validCode;
+    }
+
+    public String getValidCodeId() {
+        return validCodeId;
+    }
+
+    public void setValidCodeId(String validCodeId) {
+        this.validCodeId = validCodeId;
+    }
+}
