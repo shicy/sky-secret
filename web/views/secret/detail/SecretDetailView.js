7a34ca9f5848dc87b26347dcdb982f9e794a1e19
shicy <shicy@mixiyun.com>
2019-11-16 17:04:06 +0800

    我的秘密详情

--- web/views/secret/detail/SecretDetailView.js
+++ web/views/secret/detail/SecretDetailView.js
@@ -6,6 +6,27 @@
 const VRender = require(__vrender);
 
 
+const $ = VRender.$;
+const UIInput = VRender.UIInput;
+const UIButton = VRender.UIButton;
+
 const SecretDetailView = VRender.UIView.extend(module, {
-	className: "view-secret-detail"
+	className: "view-secret-detail",
+
+	renderView: function () {
+		SecretDetailView.super(this);
+		this.$el.addClass("empty");
+
+		let titleView = $(".title").appendTo(this.$el);
+		// new UIInput(this, {prompt: "请输入标题..."}).render(titleView);
+		$("input").appendTo(titleView).attr("placeholder", "请输入标题...");
+
+		let contentView = $(".content").appendTo(this.$el);
+		$("textarea").appendTo(contentView).attr("placeholder", "请输入...");
+
+		new UIButton(this, {name: "savebtn", label: "保存", 
+			type: "primary", icon: "/img/a/007b.png"}).render(this.$el);
+
+		$(".empty").appendTo(this.$el);
+	}
 });
