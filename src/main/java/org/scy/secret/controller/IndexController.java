fb460d2d908556dc7d62208517240134fde94528
shicy <shicy85@163.com>
2020-01-21 17:04:49 +0800

    添加 app.version 配置项

--- src/main/java/org/scy/secret/controller/IndexController.java
+++ src/main/java/org/scy/secret/controller/IndexController.java
@@ -4,10 +4,18 @@ import org.scy.common.web.controller.BaseController;
 import org.scy.common.web.controller.HttpResult;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@SuppressWarnings("unused")
 public class IndexController extends BaseController {
+
+    @RequestMapping(value = "/version", method = RequestMethod.GET)
+    @ResponseBody
+    public Object version() {
+        return HttpResult.ok(getAppVersion());
+    }
 
     @RequestMapping("/test/auth")
     @ResponseBody
