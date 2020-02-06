fd4b8b0e33fa4baa8e7fd704091e3471a9d1a984
shicy <shicy@mixiyun.com>
2020-02-06 11:56:17 +0800

    表 catalog 添加 parentIds 字段

--- src/main/resources/org/scy/secret/scripts/[1.0.0]create_v1.0.sql
+++ src/main/resources/org/scy/secret/scripts/[1.0.0]create_v1.0.sql
@@ -6,27 +6,28 @@
 
 -- USE `db_secret`;
 
--- -----------------------------------------------------
+-- --------------------------------------------------------
 -- Table `secret`.`catalog`
 -- 目录表
--- -----------------------------------------------------
+-- --------------------------------------------------------
 CREATE TABLE IF NOT EXISTS `catalog` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(100) NOT NULL COMMENT '目录名称',
   `type` TINYINT NOT NULL DEFAULT 0,
   `parentId` INT NOT NULL DEFAULT 0,
   `userId` INT NOT NULL COMMENT '所属用户编号',
+  `parentIds` VARCHAR(400) NULL COMMENT '所有上级目录编号，逗号分隔',
   `creatorId` INT NULL,
   `createTime` BIGINT NULL,
   `updatorId` INT NULL,
   `updateTime` BIGINT NULL,
   PRIMARY KEY (`id`))
-COMMENT = '目录表'
+COMMENT = '目录表';
 
--- -----------------------------------------------------
+-- --------------------------------------------------------
 -- Table `secret`.`secrets`
 -- 加密内容表
--- -----------------------------------------------------
+-- --------------------------------------------------------
 CREATE TABLE IF NOT EXISTS `secrets` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(200) NOT NULL,
@@ -38,4 +39,8 @@ CREATE TABLE IF NOT EXISTS `secrets` (
   `updatorId` INT NULL,
   `updateTime` BIGINT NULL,
   PRIMARY KEY (`id`))
-COMMENT = '加密内容'
+COMMENT = '加密内容';
+
+-- ========================================================
+-- catalog 表添加 parentIds 列 <2020-02-05 21:00:00>
+ALTER TABLE `catalog` ADD COLUMN `parentIds` VARCHAR(400) NULL COMMENT '所有上级目录编号，逗号分隔' AFTER `parentId`;
